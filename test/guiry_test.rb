af10a4838c939782dd61613074b5936ac935a5b6
Gard Honningsø <gard@honningso.com>
2017-12-17 20:55:49 +0100

    chore: setup simple test and ignore file

--- test/guiry_test.rb
+++ test/guiry_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class GuiryTest < Minitest::Test
   def test_that_it_has_a_version_number
@@ -6,6 +6,11 @@ class GuiryTest < Minitest::Test
   end
 
   def test_it_does_something_useful
+    b = 'something'
+    puts "##############"
+    puts b.slice(2, 3)
+    a = 4
+    assert_equal a + 3, 6, 'addition did not work'
     assert false
   end
 end
